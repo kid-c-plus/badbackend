b44c4a245a0fca4c95ba335538bbcf8664a06835
Eric Lewis <ericlewiswork@gmail.com>
2021-03-06 14:16:13 -0600

    finished Server class docstring

--- badbackend/core/server.py
+++ badbackend/core/server.py
@@ -1,4 +1,9 @@
 import flask
 
 class Server(flask.Flask):
-    """Provides wrapper around Flask application allowing 
+    """Provides wrapper around Flask application allowing services to 
+    easily declare public and access-controlled server endpoints using
+    the `public_endpoint` and `admin_endpoint` function decorators,
+    respectively"""
+
+    # TODO: Implement server
